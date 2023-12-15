75e2ea05bce866e8fc834a0948cf77f5021f0bd9
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-12-15 08:38:45 +0100

    resolve 01-color-switcher task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', event => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  event.currentTarget.setAttribute('disabled', 'disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
