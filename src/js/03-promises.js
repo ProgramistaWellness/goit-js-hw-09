84966fe92fc610d69b07ea0faae69449f28697f2
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-12-20 01:11:20 +0100

    Added logic for 03-promise.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+const form = document.querySelector('.form');
+
+const handleForm = event => {
+  event.preventDefault();
+  const firstDelay = parseInt(form.elements.delay.value, 10);
+  const stepDelay = parseInt(form.elements.step.value, 10);
+  const amount = parseInt(form.elements.amount.value, 10);
+  setTimeout(() => {
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, stepDelay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  }, firstDelay);
+};
+
+form.addEventListener('submit', handleForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay }); // Fulfill
+    } else {
+      reject({ position, delay }); // Reject
+    }
+  });
 }
